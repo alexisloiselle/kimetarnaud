7cfab31629c532c415eede1fb7be9ede3c5ad1d6
Alexis Loiselle <alexis@t-b.ca>
2025-01-25 17:53:39 -0500

    revert vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
-  base: '/kimetarnaud/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
