fe5c17678edb9990f315e81a08068fdf1a11298a
Alexis Loiselle <alexis.loiselle@getmaintainx.com>
2025-08-23 10:02:55 -0400

    PDF and Image

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="self-center lg:max-w-[32rem] max-w-[24rem] rounded-t-full overflow-hidden">
-    <img src="../assets/photo.png" />
+    <img
+      src="https://coydypbyhlqsxwnkpubs.supabase.co/storage/v1/object/public/assets/nouvelle%20image%20K%20et%20A%20vespa.jpg"
+    />
   </div>
 </template>
--- src/sections/MenuSection.vue
+++ src/sections/MenuSection.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import SectionLayout from './SectionLayout.vue'
-import SectionContent from './SectionContent.vue'
+import SectionLayout from '@/sections/SectionLayout.vue'
+import SectionContent from '@/sections/SectionContent.vue'
+
+const menuUrl =
+  'https://coydypbyhlqsxwnkpubs.supabase.co/storage/v1/object/public/assets/Menu%20mariage.pdf'
 
 type Props = {
   id: string
@@ -11,9 +14,20 @@ defineProps<Props>()
 
 <template>
   <SectionLayout :id="id">
-    <SectionContent class="text-dark-green">
+    <SectionContent>
       <h2>Menu</h2>
-      À venir
+      <div class="flex flex-col items-center justify-center">
+        <a :href="menuUrl" target="_blank" class="button text-xl border-2 rounded-md p-2">
+          Voir le menu
+        </a>
+      </div>
     </SectionContent>
   </SectionLayout>
 </template>
+
+<style scoped>
+.button:hover,
+.button:active {
+  background-color: var(--gray);
+}
+</style>
